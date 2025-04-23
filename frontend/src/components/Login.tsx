7fa4ccb6a1d9362485a23e23624e85117661b2be
luohy15 <luohycs@gmail.com>
2025-04-23 12:14:10 +0800

    update demo

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -26,7 +26,7 @@ export default function Login() {
         <div className="flex flex-col space-y-4">
           {/* Demo Link */}
           <a
-            href="/share/09952e1b"
+            href="/share/59a01e61"
             target="_blank"
             rel="noopener noreferrer"
             className="w-full flex items-center justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 transition-all duration-200 mb-2"
